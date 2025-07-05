9d0c9cbc7a941614767fa841cebbad0e865234b1
ricnef2121 <ricnef2121@gmail.com>
2025-07-05 00:00:09 -0600

    feat: update if else file, add OR, diff, and conditional And

--- app/src/main/java/com/example/androidmaster/Fun-If-else.kt
+++ app/src/main/java/com/example/androidmaster/Fun-If-else.kt
@@ -1,8 +1,44 @@
 package com.example.androidmaster
 
 fun main() {
-    ifBasic()
+    // ifBasic()
+    // ifAnidado()
+    // ifBoolean()
+    // ifInt()
+    // ifMultiple()
+    ifMultipleOR()
+}
 
+fun ifMultipleOR() {
+    var pet = "cat"
+    if (pet == "dog" || pet == "cat") {
+        print("Es un gato o un perro")
+    }
+}
+
+fun ifMultiple() {
+    var age = 18
+    var parentPermission = true
+    var imHappy = true
+    if (age >= 18 && parentPermission && imHappy) {
+        println("puedo beber cerveza")
+    }
+}
+
+fun ifInt() {
+    var age = 18
+    if (age >= 18) {
+        println("Beber Cerveza")
+    } else {
+        println("Beber Jugo")
+    }
+}
+
+fun ifBoolean() {
+    var soyFeliz: Boolean = true
+    if (!soyFeliz) {
+        println("Estoy triste")
+    }
 }
 
 fun ifBasic() {
@@ -12,5 +48,18 @@ fun ifBasic() {
         println("La variable es ricnef")
     } else {
         println("la variable no es ricnef")
+    }
+}
+
+fun ifAnidado() {
+    val animal = "dog"
+    if (animal == "dog") {
+        println("Es un perro")
+    } else if (animal == "cat") {
+        println("Es un gatito")
+    } else if (animal == "bird") {
+        println("Es un pajaro")
+    } else {
+        println("No es un animal")
     }
 }
