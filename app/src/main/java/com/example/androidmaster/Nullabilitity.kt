0e27e69a1adafec1dd6abe27991513a552e1d862
ricnef2121 <ricnef2121@gmail.com>
2025-07-06 17:28:56 -0600

    feat: add Null example

--- app/src/main/java/com/example/androidmaster/Nullabilitity.kt
+++ app/src/main/java/com/example/androidmaster/Nullabilitity.kt
@@ -1,5 +1,10 @@
 package com.example.androidmaster
 
-fun main(){
+fun main() {
+    var name: String? = null
+    var secondName: String = "Name"
 
+    println(name?.get(2) ?: "Lo siento no se puede acceder a la poscion en un valor nullo")
+    println(secondName.get(2))
 }
+
