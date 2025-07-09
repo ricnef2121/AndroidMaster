e9b400d47423433d70dcc4d232ecfd190277a231
ricnef2121 <ricnef2121@gmail.com>
2025-07-09 02:27:49 -0600

    feat: use AppCompatButton, AppCompatEditText, get values from there and log the values

--- app/src/main/java/com/example/androidmaster/firstApp/FirstAppActivity.kt
+++ app/src/main/java/com/example/androidmaster/firstApp/FirstAppActivity.kt
@@ -1,8 +1,11 @@
 package com.example.androidmaster.firstApp
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatButton
+import androidx.appcompat.widget.AppCompatEditText
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.androidmaster.R
@@ -12,10 +15,22 @@ class FirstAppActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_first_app)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        /* ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+             insets
+         }
+         */
+
+        val btnClick = findViewById<AppCompatButton>(R.id.btnClick)
+        val etName = findViewById<AppCompatEditText>(R.id.etName)
+
+        btnClick.setOnClickListener {
+            val name = etName.text.toString()
+            if(name.isNotEmpty()){
+                Log.i("ricnef", "Button pulsado -> ${name}")
+            }
+
         }
     }
 }
