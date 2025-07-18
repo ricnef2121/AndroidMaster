c764ebf69228dc5d1cce72a9f4ffa06e81583f3a
ricnef2121 <ricnef2121@gmail.com>
2025-07-18 01:20:52 -0600

    feat: Add MenuActivity, create button and logic to redirect at Saludapp

---
+++ app/src/main/java/com/example/androidmaster/MenuActivity.kt
@@ -0,0 +1,32 @@
+package com.example.androidmaster
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.androidmaster.firstApp.FirstAppActivity
+
+class MenuActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_menu)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val btnSaludApp = findViewById<Button>(R.id.btnSaludApp)
+        btnSaludApp.setOnClickListener { navigateToSaludApp() }
+
+    }
+
+    private fun navigateToSaludApp() {
+        val intent = Intent(this, FirstAppActivity::class.java)
+        startActivity(intent)
+    }
+}
